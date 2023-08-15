8f4a486f557c81c84aab60592e696406e32e4ce0
jonathan catriel quintana <111572681+catrielquin@users.noreply.github.com>
2023-08-15 20:06:44 -0300

    Add files via upload

---
+++ Leccion07/capa_datos_persona/Logger_base.py
@@ -0,0 +1,22 @@
+# docs.python.org/howto/logging.html, página web para la configuración de logging
+
+import logging as log
+
+# log = logging, también podría hacerse
+
+
+# Llamamos una configuracion basica
+log.basicConfig(level=log.INFO,  # %s = parámetro posicional
+                format='%(asctime)s:%(levelname)s [%(filename)s:%(lineno)s] %(message)s',
+                datefmt='%I:%M:%S %p',  # datefmt = date format
+                handlers=[
+                    log.FileHandler('capa_datos.log', encoding='utf8'),
+                    log.StreamHandler()
+                ])
+
+if __name__ == '__main__':
+    log.debug('Mensaje a nivel debug')
+    log.info('Mensaje a nivel info')
+    log.warning('Mensaje a nivel warning')
+    log.error('Mensaje a nivel error')
+    log.critical('Mensaje a nivel critical')
