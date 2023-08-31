39ce674329cfc5909340c29ba979a72ced2a1bb9
jonathan catriel quintana <111572681+catrielquin@users.noreply.github.com>
2023-08-30 21:29:47 -0300

    Add files via upload
    agrego el código de la clase 03 de Node

---
+++ tecnicaturagit4Node/01-Hola-Mundo/app2.js
@@ -0,0 +1,4 @@
+const saludar = (nombre) =>{ // funcion de flecha
+    return `Saludos de ${nombre}`; // usamos el back tic`s
+} // esto es igual a: saludo = +nombre
+console.log(saludar("jonathan"));
