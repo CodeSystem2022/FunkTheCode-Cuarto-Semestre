dd8b1aecbbb4fcd27526619a3f5ffa43c42cc3e7
jonathan catriel quintana <111572681+catrielquin@users.noreply.github.com>
2023-09-18 20:34:43 -0300

    Add files via upload
    incorporo lo visto en la clase 05 de python

---
+++ tecnicaturagit4Python/Clase01/Clase01/tipo_str.py
@@ -0,0 +1,13 @@
+
+
+# profundizando en el tipo String
+# Concatenacion automatica en Python
+import math
+
+variable = " Adios"
+mensaje = "Hola " "Alumno" + variable
+mensaje += ", Terminamos"
+#print(mensaje)
+
+# Usamos la clase help para ayuda o documentacion (built-in)
+help(math.isnan)
