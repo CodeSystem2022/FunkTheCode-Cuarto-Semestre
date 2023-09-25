f13e6093af4580af5b037c4fef5e2498d83a2950
jonathan catriel quintana <111572681+catrielquin@users.noreply.github.com>
2023-09-25 20:21:56 -0300

    Add files via upload

---
+++ tecnicaturagit4JavaScript/Leccion01/Semana6/PERN-stack/database/controllers/auth.controller.js
@@ -0,0 +1,8 @@
+export const signin = (req, res) => res.send('ingresando');
+
+export const signup = (req, res) => res.send('registrando');
+
+export const signout = (req, res) => res.send('Cerrando sesion');
+
+export const profile = (req, res) => res.send('Perfil del usuario');
+
