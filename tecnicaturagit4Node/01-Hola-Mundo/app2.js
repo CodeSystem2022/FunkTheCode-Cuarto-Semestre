e471375f97b0db0f49ade84fa646c906b63025c6
Danilo <danoprofi11@gmail.com>
2023-10-02 14:05:59 -0300

    Node hasta la clase 07

--- tecnicaturagit4Node/01-Hola-Mundo/app2.js
+++
@@ -1,4 +0,0 @@
-const saludar = (nombre) =>{ // funcion de flecha
-    return `Saludos de ${nombre}`; // usamos el back tic`s
-} // esto es igual a: saludo = +nombre
-console.log(saludar("jonathan"));
