c6263129af2024e9489b7f67bedfc7245fef97e1
jonathan catriel quintana <111572681+catrielquin@users.noreply.github.com>
2023-10-19 22:45:36 -0300

    Add files via upload
    actualización del código clase 09 tienda libro

--- tecnicaturagit4Java/Leccion03/tienda_libros/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
+++ tecnicaturagit4Java/Leccion03/tienda_libros/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
@@ -1,13 +1,32 @@
 package utn.tienda_libros;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import utn.tienda_libros.modelo.Libro;
+import utn.tienda_libros.vista.LibroFrom;
+
+import java.awt.EventQueue;
 
 @SpringBootApplication
 public class TiendaLibrosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TiendaLibrosApplication.class, args);
-	}
 
+		ConfigurableApplicationContext contextoSpring =
+				new SpringApplicationBuilder(TiendaLibrosApplication.class)
+						.headless(false)
+						.web(WebApplicationType.NONE)
+						.run(args);
+
+
+		//Ejecutamos el codigo para cargar el formulario
+		EventQueue.invokeLater(() -> {  // Metodo Lambda
+			//Obtenemos el objeto from a travez del spring
+			LibroFrom libroFrom = contextoSpring.getBean(LibroFrom.class);
+			libroFrom.setVisible(true);
+		});
+	}
 }
